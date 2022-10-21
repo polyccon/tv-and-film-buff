20c1d981154faff065f4a24e7bb80a4d462bc4fb
Constantina Polycarpou <polyccon@gmail.com>
2022-10-21 17:54:05 +0100

    feat: adds custom filter for imdb_rating greater than or equal

--- tv_and_film_buffAPI/filters.py
+++ tv_and_film_buffAPI/filters.py
@@ -13,3 +13,12 @@ class EpisodesListFilter(django_filters.FilterSet):
         model = Episodes
         fields = ["imdb_rating", "series", "genre", "language"]
 
+    imdb_rating = django_filters.CharFilter(
+        method="imdb_rating_filter",
+        label="imdb_rating",
+        help_text="Filter for imdb_rating greater than or equal"
+    )
+
+    def imdb_rating_filter(self, qs, name, value):
+        return qs.filter(imdb_rating__gte=value)
+
