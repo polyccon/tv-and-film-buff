198f57acae6b53564f2eee311c2827e1f9ee772a
Constantina Polycarpou <polyccon@gmail.com>
2022-11-25 00:31:42 +0000

    test: adds test case for delete comment endpoint

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -13,7 +13,7 @@ from tests.factories import (
     CommentsFactory,
 )
 from tv_and_film_buffAPI.models import Comments
-from tv_and_film_buffAPI.urls import EPISODES_LIST, EPISODE_RETRIEVE, COMMENTS_LIST_CREATE
+from tv_and_film_buffAPI.urls import EPISODES_LIST, EPISODE_RETRIEVE, COMMENTS_LIST_CREATE, COMMENTS_RETRIEVE_UPDATE
 
 
 @pytest.fixture
@@ -38,6 +38,7 @@ def test_episodes_list_endpoint_returns_episodes(episode):
     url = reverse(
         viewname=EPISODES_LIST.name,
     )
+    
     response = client.get(url, format="json")
 
     assert response.status_code == 200
@@ -62,6 +63,7 @@ def test_episodes_list_endpoint_returns_episodes(episode):
 def test_episodes_get_endpoint_returns_episode(episode):
     client = APIClient()
     url = reverse(viewname=EPISODE_RETRIEVE.name, kwargs={"imdb_id": "tt1480055"})
+    
     response = client.get(url, format="json")
 
     assert response.status_code == 200
@@ -84,6 +86,7 @@ def test_episodes_get_endpoint_returns_episode(episode):
 def test_episodes_get_endpoint_returns_404_for_non_existent_id(episode):
     client = APIClient()
     url = reverse(viewname=EPISODE_RETRIEVE.name, kwargs={"imdb_id": "tt1480052"})
+    
     response = client.get(url, format="json")
 
     assert response.status_code == 404
@@ -93,7 +96,9 @@ def test_episodes_get_endpoint_returns_404_for_non_existent_id(episode):
 def test_comments_list_endpoint_returns_comments(comment):
     client = APIClient()
     url = reverse(viewname=COMMENTS_LIST_CREATE.name, kwargs={"imdb_id": "tt1480055"})
+    
     response = client.get(url, format="json")
+    
     assert response.status_code == 200
     assert response.json() == [
         {"body": "This is a comment for an episode", "episode": comment.episode.pk}
@@ -106,11 +111,26 @@ def test_comments_create_endpoint_adds_and_returns_comment(episode, comment):
     assert comments.count() == 1
     client = APIClient()
     url = reverse(viewname=COMMENTS_LIST_CREATE.name, kwargs={"imdb_id": "tt1480055"})
+    
     response = client.post(
         url,
         {"body": "this is a test comment", "episode": episode.pk},
         format="json"
     )
+    
     assert response.status_code == 201
     assert response.json() == {"body": "this is a test comment", "episode": episode.pk}
     assert comments.count() == 2
+
+
+@pytest.mark.django_db
+def test_comments_delet_endpoint_deletes_comment(episode, comment):
+    comments = Comments.objects.filter(episode=episode)
+    assert comments.count() == 1
+    client = APIClient()
+    url = reverse(viewname=COMMENTS_RETRIEVE_UPDATE.name, kwargs={"imdb_id": "tt1480055", "id": comment.pk})
+    
+    response = client.delete(url,format="json")
+    
+    assert response.status_code == 204
+    assert comments.count() == 0
