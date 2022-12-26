bb6c8c4d167e5f70de12e83b1f93c2f9799ffc61
Constantina Polycarpou <polyccon@gmail.com>
2022-12-26 19:02:25 +0000

    deps: adds corsheaders middleware

--- tv_and_film_buffAPI/config/settings.py
+++ tv_and_film_buffAPI/config/settings.py
@@ -67,6 +67,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "tv_and_film_buffAPI.urls"
