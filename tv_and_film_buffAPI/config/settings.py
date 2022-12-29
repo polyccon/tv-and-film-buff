42f3061e6764c4411dea24b6639caf6ebf1f542d
Constantina Polycarpou <polyccon@gmail.com>
2022-12-29 15:02:26 +0000

    fix: adds corsheaders missing middleware

--- tv_and_film_buffAPI/config/settings.py
+++ tv_and_film_buffAPI/config/settings.py
@@ -60,8 +60,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
